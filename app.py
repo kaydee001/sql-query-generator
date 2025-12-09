e68ae2aa3447f0b79f8fbbf287a3cbfb0f1ac830
Kartik Dhasmana <54183908+kaydee001@users.noreply.github.com>
2025-12-09 11:08:33 +0530

    database schema viewer

--- app.py
+++ app.py
@@ -5,6 +5,25 @@ import pandas as pd
 st.title("SQL Query Generator")
 sql_query = st.text_area("Enter SQL query", "SELECT * FROM customers")
 
+with st.expander("View database : "):
+    conn = sqlite3.connect("database/sales.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = cursor.fetchall()
+
+    for table in tables:
+        table_name = table[0]
+        st.subheader(f"Table : {table_name}")
+
+        cursor.execute(f"PRAGMA table_info({table_name})")
+        columns = cursor.fetchall()
+
+        for col in columns:
+            st.text(f"  • {col[1]} ({col[2]})")
+
+    conn.close()
+
 if st.button("Run Query"):
     conn = sqlite3.connect("database/sales.db")
     cursor = conn.cursor()
